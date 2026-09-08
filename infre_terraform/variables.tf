208a02a0e6330ce7dd02c14e2f73d1e061f09242
Sagar Bhond <YOUR_GITHUB_EMAIL>
2026-09-08 07:25:08 +0530

    Fix inventory H2 database deployment

--- infre_terraform/variables.tf
+++ infre_terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type for application server."
   type        = string
-  default     = "c7i.large"
+  default     = "c7i-flex.large"
 }
 
 variable "monitoring_instance_type" {
